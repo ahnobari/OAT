4454b81d65f205209247c2fd4ff7eb944519775c
ahnobari <nobariam@gmail.com>
2025-05-06 20:10:38 -0400

    latent naming

--- ComputeLatents.py
+++ ComputeLatents.py
@@ -43,6 +43,7 @@ trainer = Trainer(model,
 
 if args.hf_checkpoint:
     model.from_pretrained(args.checkpoint)
+    latest_checkpoint = args.checkpoint.replace("/", "__")
     
 elif os.path.exists(args.checkpoint):
     model.load_from_trainer_checkpoint(args.checkpoint, strict=False)
